1f1cf7f8c547ac35069ed201aa660b0baef61a48
jolongnong <jolongnong@naver.com>
2024-07-25 15:34:25 +0900

    style : 시큐리티 수정

--- src/main/java/com/project/pickyou/config/SecurityConfig.java
+++ src/main/java/com/project/pickyou/config/SecurityConfig.java
@@ -46,12 +46,16 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests((auth)->auth
 
-                        .requestMatchers("/","/login","/joinCompany","/forgot/**","/mail/**","/agency/**","/trainning/**","/posts/**","/join","/joinProc","/assets/**", "/img/**","/register","/css/**", "/js/**").permitAll()
-                        .requestMatchers("/login","/ajax/**","/forgotId/**","/forgotPw/**","/mypage/**","/notice/**","/payProcess/**").permitAll()
+                        .requestMatchers("/","/login","/joinCompany","/forgot/**","/mail/**","/posts/**","/join","/joinProc","/assets/**", "/img/**","/register","/css/**", "/js/**").permitAll()
+                        .requestMatchers("/login","/ajax/**","/forgotId/**","/forgotPw/**","/notice/**","/payProcess/**").permitAll()
                         .requestMatchers("/educations/**","/qa/**").permitAll()
                         .requestMatchers("/recruit/**").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
+
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/trainning/posts/new","/agency/posts/new","/agency/posts/*/edit","/trainning/posts/*/edit").hasRole("COMPANY")
+                        .requestMatchers("/agency/**","/trainning/**").permitAll()
                         .requestMatchers("/my/**").hasAnyRole("USER","ADMIN")
+                        .requestMatchers("/mypage/**").hasAnyRole("USER","ADMIN","COMPANY")
                         .anyRequest().permitAll()
 
 
